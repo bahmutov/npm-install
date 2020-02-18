0f652189939592385015baafacc1ed268ada3e06
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-18 10:36:51 -0500

    first test

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,5 @@
 const npmInstall = require('../index')
+const utils = npmInstall.utils
 
 it('exports a function', () => {
   expect(npmInstall)
@@ -10,7 +11,26 @@ it('exports a function', () => {
 it('cache was not hit', async () => {
   // previous cache not found
   const cacheHit = false
-  sandbox
-    .stub(npmInstall.utils, 'restoreCachedNpm')
-    .resolves(cacheHit)
+  const restoreCache = sandbox.stub(
+    utils,
+    'restoreCachedNpm'
+  )
+  const install = sandbox.stub(utils, 'install')
+  const saveCache = sandbox.stub(utils, 'saveCachedNpm')
+
+  restoreCache.resolves(cacheHit)
+  install.resolves()
+  saveCache.resolves()
+
+  await npmInstall.npmInstallAction()
+  expect(saveCache, 'new cache was saved').to.have.been
+    .calledOnce
+  expect(
+    restoreCache,
+    'restore cache was checked first'
+  ).to.have.been.calledBefore(install)
+  expect(
+    install,
+    'install was called before saving cache'
+  ).to.have.been.calledBefore(saveCache)
 })
